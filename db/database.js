8633d7cc3aa9714016fa66ad1b799d446d2466ff
Thomas Swann <tkswann@mac.com>
2016-09-16 13:05:18 -0500

    added comments to database file for clarity

--- db/database.js
+++ db/database.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 // const MONGODB_URL = `mongodb://tkswann2:${pw}@ds033076.mlab.com:33076/reddit-clone`
 const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/redditclone'
 
+// sets mongoose promises to native es6 promise library
 mongoose.Promise = Promise
 
 module.exports.connect = () => mongoose.connect(MONGODB_URL)
