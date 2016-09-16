96cf529e7cb307cded6b75de08a88af53e91be07
Thomas Swann <tkswann@mac.com>
2016-09-16 11:26:34 -0500

    more heroku deployment

--- db/database.js
+++ db/database.js
@@ -1,6 +1,5 @@
 'use strict'
 const mongoose = require('mongoose')
-const pw = require('./yodawg')
 
 // const MONGODB_URL = `mongodb://tkswann2:${pw}@ds033076.mlab.com:33076/reddit-clone`
 const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/redditclone'
