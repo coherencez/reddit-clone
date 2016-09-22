fb6d0d53e637cc52ad491c71f3cfec203e5367cb
Thomas Swann <tkswann@mac.com>
2016-09-22 11:16:57 -0500

    abstracted comments logic to controller

---
+++ controllers/comments.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const Article = require('../models/article')
+
+module.exports.new = ({params: {id}},res) => {
+  Article
+    .findOne({_id: id})
+    .then(article => {
+      article.comments = article.comments.sort((a,b) => {
+        if(a.vote < b.vote) return  1
+        if(a.vote > b.vote) return -1
+      })
+      res.render('comments', {article})
+    })
+}
+
+module.exports.create = ({body, params: {id}},res, cb) => {
+  if(body.submitButton) {
+    let newObj = {
+      text: body.comments,
+      vote: 0,
+      id: new Date()
+    }
+    Article
+      .update({_id: id}, {$push: { comments: newObj } })
+      .then(() => res.redirect(`/comments/${id}`))
+      .catch(cb)
+    } else {
+      let voteIncrementer
+       ,         voteType
+
+      if(Object.keys(body)[0] === 'upvote') {
+        voteIncrementer = 1
+        voteType = 'upvote' 
+      } else {
+        voteIncrementer = -1
+        voteType = 'downvote'
+      }
+      Article
+        .update({_id: id, "comments": { $elemMatch: { "id": body[voteType]} } }, { "$inc": { "comments.$.vote": voteIncrementer}} )
+        .then(() => res.redirect(`/comments/${id}`))
+        .catch(cb)
+  }
+}
